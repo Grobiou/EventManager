da0f79fcd1dd7c3f7863038c563cc70db6639f8f
Guarionex Robiou <Guarionex Robiou>
2017-04-24 10:37:25 -0400

    Added camera support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,7 +28,7 @@ import { ProfileProvider } from '..\providers\profile/profile';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}, AuthProvider, EventProvider, ProfileProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, AuthProvider, EventProvider, ProfileProvider, Camera
   ]
 })
 export class AppModule {}
