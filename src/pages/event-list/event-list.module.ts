455bb1eb45fa55e565d5639858de3d351900a543
Guarionex Robiou <Guarionex Robiou>
2017-04-24 10:18:33 -0400

    Changed IonicModule to IonicPageModule

---
+++ src/pages/event-list/event-list.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EventListPage } from './event-list';
+
+@NgModule({
+  declarations: [
+    EventListPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EventListPage),
+  ],
+  exports: [
+    EventListPage
+  ]
+})
+export class EventListPageModule {}
