fbcc7572cb7bc32676c9e03226e4fa2a43575a74
Guarionex Robiou <Guarionex Robiou>
2017-04-27 19:52:38 -0400

    Added the add guests feature

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,1 +1,8 @@
-﻿import { Component } from '@angular/core';import { IonicPage, NavController, NavParams } from 'ionic-angular';import { EventProvider } from '../../providers/event/event';@IonicPage({    name: 'event-detail',    segment: 'event-detail/:eventId'})@Component({    selector: 'page-event-detail',    templateUrl: 'event-detail.html',})export class EventDetailPage {    public currentEvent: any;    constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider) {    }    ionViewDidEnter() {        this.eventProvider.getEventDetails(this.navParams.get('eventId'))            .then(eventSnap => {                this.currentEvent = eventSnap;            })    }}
+﻿import { Component } from '@angular/core';import { IonicPage, NavController, NavParams } from 'ionic-angular';import { EventProvider } from '../../providers/event/event';@IonicPage({    name: 'event-detail',    segment: 'event-detail/:eventId'})@Component({    selector: 'page-event-detail',    templateUrl: 'event-detail.html',})export class EventDetailPage {    public currentEvent: any;    constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider) {    }    ionViewDidEnter() {        this.eventProvider.getEventDetails(this.navParams.get('eventId'))            .then(eventSnap => {                this.currentEvent = eventSnap;            })    }
+
+    addGuest(guestName, guestPicture) {
+        this.eventProvider.addGuest(guestName, this.currentEvent.id, this.currentEvent.price, guestPicture)
+            .then(() => {
+                guestName = '';
+            });
+    }}
